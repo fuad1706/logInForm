a64e94edc3ec8c02a31fe0b2be98359cdd2824c5
fuad1706 <fuadshorlarxwix@gmail.com>
2024-02-19 17:54:01 -1200

    javascript-sec

---
+++ index.js
@@ -0,0 +1,7 @@
+function validateLoginInput(input){
+    const usernameRegex = /^[a-zA-Z0-9]{6,}$/;
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    const phoneRegex = /^\d{10}$/;
+
+    if(usernameRegex)
+}
